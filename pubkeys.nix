503e54b3fe9bbe6cda666ea472fe530daabd30af
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-02-01 13:07:21 +0100

    Create pubkeys.nix

---
+++ pubkeys.nix
@@ -0,0 +1,7 @@
+{
+  "desktop" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPwZL27kGTQDIlSe03abT9F24nSAizORyjo5cI3BD92s your_email@example.com";
+  "laptop" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJ6+aLTPanIYS88EjCVtCZv6pw2jC4lIIZNRY6VrnoF pungkula@laptop";
+  "nasty" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLU9Ri6EVsKMHMXm1L5N0sU9qUVrQDgmC+o6vJnik9u pungis@nasty";
+}
+
+
